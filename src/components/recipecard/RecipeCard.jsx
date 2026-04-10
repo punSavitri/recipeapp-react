1caadf316fb9ba80415edc64940ba282771bf606
SavitriPun <punshakshi@gmail.com>
2026-04-10 08:50:56 +0100

    created RecipeCard component, mount to App.js

---
+++ src/components/recipecard/RecipeCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './RecipeCard.css';
+
+const RecipeCard = () => {
+  return (
+    <div className='recipecard'>RecipeCard</div>
+  )
+}
+
+export default RecipeCard
