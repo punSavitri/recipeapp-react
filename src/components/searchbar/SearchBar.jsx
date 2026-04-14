f51e8c38151f90b109c2fd2ec19c33abaae87006
SavitriPun <punshakshi@gmail.com>
2026-04-14 22:56:11 +0100

    IImplemented SearchBar component with controlled text input and disabled button states

--- src/components/searchbar/SearchBar.jsx
+++ src/components/searchbar/SearchBar.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import "./SearchBar.css";
 
-const SearchBar = (props) => {
+const SearchBar = ({ value, isLoading, handleSubmit, onChange }) => {
   return (
     <div className="searchbar">
       <form>
         <input
           type="text"
+          value={value}
+          disabled={isLoading}
           placeholder="Find a recipe..."
           className="form-control"
+          onChange={onChange}
         />
-        <input type="submit" className="btn" value="Search" />
+        <input
+          type="submit"
+          className="btn"
+          value="Search"
+          disabled={isLoading || !value}
+        />{" "}
+        //disabled button if it is Loading or input is empty
       </form>
     </div>
   );
