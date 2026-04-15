53156a7e327b62e7e7a8ca1c34fc909842979844
SavitriPun <punshakshi@gmail.com>
2026-04-15 23:36:18 +0100

    created Footer component and mount to App.js

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>
+        This project was developed by{" "}
+        <a
+          href="https://github.com/punSavitri"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Savitri Pun
+        </a>
+        available on{" "}
+        <a
+          href="https://github.com/punSavitri/recipeapp-react.git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          as open-source on GitHub repository
+        </a>{" "}
+        and deployed using Hostinger platform
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
